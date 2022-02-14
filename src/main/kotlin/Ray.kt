d3aeadebe9d4a0df161c52fa5b0e2f0e4ab77bec
Dan Noonan <dan@noonan.io>
2022-02-13 19:36:25 -0600

    In One Weekend - 4.1. The ray Class

---
+++ src/main/kotlin/Ray.kt
@@ -0,0 +1,8 @@
+data class Ray(
+    @JvmField val origin: Point3 = Point3(),
+    @JvmField val direction: Vec3 = Vec3()
+) {
+    fun at(t: Double): Vec3 {
+        return origin + t * direction
+    }
+}
