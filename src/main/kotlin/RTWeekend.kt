1f971c4ec246ba8dafa72d8b0e99542b4534f739
Dan Noonan <dan@noonan.io>
2022-02-16 20:21:11 -0600

    In One Weekend - 7.1. Some Random Number Utilities

--- src/main/kotlin/RTWeekend.kt
+++ src/main/kotlin/RTWeekend.kt
@@ -1,3 +1,5 @@
+import java.util.Random
+
 // Constants
 
 const val infinity = Double.POSITIVE_INFINITY
@@ -7,4 +9,16 @@ const val pi = Math.PI
 
 inline fun degreesToRadians(degrees: Double): Double {
     return degrees * pi / 180.0
+}
+
+val random = Random(0)
+
+inline fun randomDouble(): Double {
+    // Returns a random real in [0, 1).
+    return random.nextDouble()
+}
+
+inline fun randomDouble(min: Double, max: Double): Double {
+    // Returns a random real in [min,max).
+    return min + (max - min) * randomDouble()
 }
